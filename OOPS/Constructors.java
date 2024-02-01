7e95f98992631f40aedccdf24f8329123f2c161e
Suhail Ahmed <84863212+suhailahmed123@users.noreply.github.com>
2024-02-01 16:07:28 +0530

    Update Constructors.java

--- OOPS/Constructors.java
+++ OOPS/Constructors.java
@@ -1,31 +1,53 @@
 /*********************************************CONSTRUCTORS****************************/
+/*
+
+- James Gosling told that he has given separate setters
+1. It doesn't have return type
+2. setters will have same name as class name and this is called as constructor
+
+so if we can the definition of constructor as, 
+- The constructor is such a method who doesn't have any return type and has same name as of class name.
+
+how to create a constructor?
+- The constructor can be created in the same way as object is created
+- Ex:- Client c = new Client();
+	Human h = new Human();
+ 	Car c = new Car();
+
+here let's understand deeper abount the constructor syntax
+---> Client - is a class name
+---> c - is a reference or in other words we can say as handle or a steering
+---> new - is a keyword used create objects
+---> Client() - is also a class name but () is a call to zero parameterized constructor
+
+Advantage:-
+1. constructors will have same name as class name
+2. constructor will not have any return type
+3. constructor will get called during/when object is created
 
 
+- IMP
+* if there is a parameterized constructor present then we have to explicitly write/create non-parameterized constructor
+
+* if there is no zero parameter constructor then there is no use of creating object of that call as it can throws error by saying {The constructor Client() is undefined]
+		Client c1 = new Client(); // Throws error [The constructor Client() is undefined]
+*/
 class Client {
 	private int id;
 	private String name;
 	private long number;
 
-	public Client() { // non-parameter constructor
+	public Client() { // non-parameter constructor or zero parameterized constructor
 
-		// it is not visible but automatically called
+		//This constructor not visible but automatically called when we are calling to non - parameter constructor or at the time of object creation
+		id = 1;
+		name = "suhail";
+		number = 7418529630L;
 	}
 
 	public Client(int id, String name, long number) { // parameterized constructor
 		this.id = id;
 		this.name = name;
-		this.number = number;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setNumber(long number) {
 		this.number = number;
 	}
 
@@ -46,25 +68,14 @@ public class Constructors {
 
 	public static void main(String[] args) {
 
-		Client c1 = new Client(); // Throws error [The constructor Customer() is undefined]
+		Client c1 = new Client(); // Throws error [The constructor Client() is undefined]
 		// Client() ---> default constructor gets called automatically
-		c1.setId(22);
-		c1.setName("Suhail");
-		c1.setNumber(7026763291L);
-
 		System.out.println(c1.getId() + " " + c1.getName() + " " + c1.getNumber());
 
-		Client c2 = new Client(); // Throws error [The constructor Customer() is undefined]
-		c2.setId(00);
-		c2.setName("Ahmed");
-		c2.setNumber(7412588520L);
+		// if there is parameterized constructor we need to create non-parameterized constructor explicitly
+
+		Client c2 = new Client(007, "James Bond", 7418529630L);
 		System.out.println(c2.getId() + " " + c2.getName() + " " + c2.getNumber());
-
-		// if there is parameterized constructor we need to create non-parameter
-		// constructor explicitly
-
-		Client c3 = new Client(007, "James Bond", 7418529630L);
-		System.out.println(c3.getId() + " " + c3.getName() + " " + c3.getNumber());
 
 	}
 }
